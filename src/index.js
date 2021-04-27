05794c4cee1299a1a59cbbb415ae251f406c0c2d
Isaac <izaroot327@gmail.com>
2021-04-27 16:36:29 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import SimpleComponent from './components/SimpleComponent';
-import SimplerComponent from './components/SimplerComponent';
+import {SimplerComponent} from './components/SimplerComponent';
 
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => console.log('hello')} />
   </div>,
   document.getElementById('root')
 );
